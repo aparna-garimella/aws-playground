390141ec317fad20e3a060e5a3e8f3fda7344dd2
Aparna Garimella <Aparna.Garimella@primeroedge.com>
2023-02-27 15:48:03 -0600

    sqs-publisher

---
+++ sqs-publisher/CustomerCreated.cs
@@ -0,0 +1,6 @@
+class CustomerCreated
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ sqs-publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var sqsClient = new AmazonSQSClient();
+var newCustomer = new CustomerCreated{
+    Id = 12,
+    Name = "Customer 1"
+};
+
+var url = await sqsClient.GetQueueUrlAsync("Customers");
+var request = new SendMessageRequest{
+    QueueUrl = url.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(newCustomer)
+};
+
+var response = await sqsClient.SendMessageAsync(request); 
